730144d240d8932674abf75310bb84d13d426a2c
Marcio <marciogomes@alu.ufc.br>
2023-06-16 21:04:56 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+// a simple java program
+// to run this program: java Main
+public class Main {
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("Ave Maria Puríssima!");
+    } // end method main
+} // end class Main
